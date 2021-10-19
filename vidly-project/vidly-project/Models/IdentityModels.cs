688c628787c6a2deb24de8388fe711f565d0046f
Matt B <mattbuckwell@gmail.com>
2021-10-19 20:22:22 +1300

    Code-first migration.

--- vidly-project/vidly-project/Models/IdentityModels.cs
+++ vidly-project/vidly-project/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace vidly_project.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
