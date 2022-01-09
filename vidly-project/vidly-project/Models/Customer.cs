c1ccc190baf22678b14fb459598e5a9ccbe102fd
Matt B <mattbuckwell@gmail.com>
2022-01-09 15:47:35 +1300

    Added basic New Customer form.

--- vidly-project/vidly-project/Models/Customer.cs
+++ vidly-project/vidly-project/Models/Customer.cs
@@ -13,6 +13,7 @@ namespace vidly_project.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
