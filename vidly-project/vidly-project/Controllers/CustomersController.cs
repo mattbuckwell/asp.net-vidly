b4ea7458a4fb36df2546eceed71ff644e383a3c1
Matt B <mattbuckwell@gmail.com>
2022-03-17 19:06:52 +1300

    Added Validation Summary to the Customer Form.

--- vidly-project/vidly-project/Controllers/CustomersController.cs
+++ vidly-project/vidly-project/Controllers/CustomersController.cs
@@ -28,6 +28,7 @@ namespace vidly_project.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
             return View("CustomerForm", viewModel);
