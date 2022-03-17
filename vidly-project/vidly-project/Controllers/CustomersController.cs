fc5f53e3b6084283c442d625e8e7cf4c725df571
Matt B <mattbuckwell@gmail.com>
2022-03-17 22:43:36 +1300

    Adding Anti-forgery Token to Customer Form.

--- vidly-project/vidly-project/Controllers/CustomersController.cs
+++ vidly-project/vidly-project/Controllers/CustomersController.cs
@@ -35,6 +35,7 @@ namespace vidly_project.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
