4f3c92f38ea787ea4d578c099bce127e110b4e91
Matt B <mattbuckwell@gmail.com>
2022-04-23 14:29:38 +1200

    Adding AutoMapper into the Project to map the objects in the Customer Api.

---
+++ vidly-project/vidly-project/App_Start/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using vidly_project.Dtos;
+using vidly_project.Models;
+
+namespace vidly_project.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Create a mapping configuration between 2 types, Generic method that takes 2 parameters - source and target
+            // when we call this Create method, automapper using refleciton to scan these types, it finds their properties
+            // and maps them based on their name - Convention Based mapping tool
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
